ed260ccf51f55e96393ffdfbafe1a8ef0fbfa76c
Attila Farago <attila.farago.hu@gmail.com>
2022-02-17 01:41:43 +0100

    slimmelezer generalized and addedd Hungary-EON/MVM specifics, hopefully to standard OBIS codes

--- components/dsmr/sensor.py
+++ components/dsmr/sensor.py
@@ -5,94 +5,160 @@ from esphome.const import (
     DEVICE_CLASS_CURRENT,
     DEVICE_CLASS_EMPTY,
     DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_GAS,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_VOLTAGE,
     ICON_EMPTY,
-    LAST_RESET_TYPE_NEVER,
     STATE_CLASS_MEASUREMENT,
     STATE_CLASS_NONE,
+    STATE_CLASS_TOTAL_INCREASING,
     UNIT_AMPERE,
+    UNIT_CUBIC_METER,
     UNIT_EMPTY,
+    UNIT_KILOWATT,
+    UNIT_KILOWATT_HOURS,
+    UNIT_KILOVOLT_AMPS_REACTIVE_HOURS,
+    UNIT_KILOVOLT_AMPS_REACTIVE,
     UNIT_VOLT,
-    UNIT_WATT_HOURS,
-    UNIT_WATT,
+    UNIT_HERTZ,
 )
-from . import DSMR, CONF_DSMR_ID
+from . import Dsmr, CONF_DSMR_ID
 
 AUTO_LOAD = ["dsmr"]
 
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_DSMR_ID): cv.use_id(DSMR),
-        cv.Optional("energy_delivered_lux"): sensor.sensor_schema(
-            "kWh",
+        cv.GenerateID(CONF_DSMR_ID): cv.use_id(Dsmr),
+        cv.Optional("energy_delivered"): sensor.sensor_schema(
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("energy_delivered_tariff1"): sensor.sensor_schema(
-            "kWh",
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("energy_delivered_tariff2"): sensor.sensor_schema(
-            "kWh",
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
-        cv.Optional("energy_returned_lux"): sensor.sensor_schema(
-            "kWh",
+        cv.Optional("energy_returned"): sensor.sensor_schema(
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("energy_returned_tariff1"): sensor.sensor_schema(
-            "kWh",
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("energy_returned_tariff2"): sensor.sensor_schema(
-            "kWh",
+            UNIT_KILOWATT_HOURS,
             ICON_EMPTY,
-            3,
+            1,
             DEVICE_CLASS_ENERGY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("total_imported_energy"): sensor.sensor_schema(
-            "kvarh", ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_NONE
+            UNIT_KILOVOLT_AMPS_REACTIVE_HOURS,
+            ICON_EMPTY,
+            1,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_NONE,
         ),
         cv.Optional("total_exported_energy"): sensor.sensor_schema(
-            "kvarh", ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_NONE
+            UNIT_KILOVOLT_AMPS_REACTIVE_HOURS,
+            ICON_EMPTY,
+            1,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_NONE,
+        ),
+        cv.Optional("electricity_tariff"): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
         ),
         cv.Optional("power_delivered"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_returned"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
+        
+        # cv.Optional("reactive_power_qi"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_power_qii"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_power_qiii"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_power_qiv"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+        # ),
+
+        # cv.Optional("reactive_energy_qi"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_energy_qii"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_energy_qiii"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+        # ),
+        # cv.Optional("reactive_energy_qiv"): sensor.sensor_schema(
+        #     UNIT_KILOVOLT_AMPS_REACTIVE_HOURS, ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+        # ),
+
+        # cv.Optional("instantaneous_power_factor"): sensor.sensor_schema(
+        #     UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
+        # ),
+        # cv.Optional("instantaneous_power_factor_l1"): sensor.sensor_schema(
+        #     UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
+        # ),
+        # cv.Optional("instantaneous_power_factor_l2"): sensor.sensor_schema(
+        #     UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
+        # ),
+        # cv.Optional("instantaneous_power_factor_l3"): sensor.sensor_schema(
+        #     UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
+        # ),
+
+        cv.Optional("frequency"): sensor.sensor_schema(
+            UNIT_HERTZ, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_MEASUREMENT
+        ),
+
+        # cv.Optional("absolute_active_energy"): sensor.sensor_schema(
+        #     UNIT_KILOWATT_HOURS, ICON_EMPTY, 1, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+        # ),
+
         cv.Optional("reactive_power_delivered"): sensor.sensor_schema(
-            "kvar", ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_NONE
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_POWER,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_returned"): sensor.sensor_schema(
-            "kvar", ICON_EMPTY, 3, DEVICE_CLASS_ENERGY, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_POWER,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("electricity_threshold"): sensor.sensor_schema(
-            UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
+            UNIT_KILOWATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER, STATE_CLASS_NONE
         ),
         cv.Optional("electricity_switch_position"): sensor.sensor_schema(
             UNIT_EMPTY, ICON_EMPTY, 3, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
@@ -107,13 +173,13 @@ CONFIG_SCHEMA = cv.Schema(
             UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
         ),
         cv.Optional("electricity_sags_l2"): sensor.sensor_schema(
-            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_MEASUREMENT
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
         ),
         cv.Optional("electricity_sags_l3"): sensor.sensor_schema(
             UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
         ),
         cv.Optional("electricity_swells_l1"): sensor.sensor_schema(
-            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_MEASUREMENT
+            UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
         ),
         cv.Optional("electricity_swells_l2"): sensor.sensor_schema(
             UNIT_EMPTY, ICON_EMPTY, 0, DEVICE_CLASS_EMPTY, STATE_CLASS_NONE
@@ -131,40 +197,64 @@ CONFIG_SCHEMA = cv.Schema(
             UNIT_AMPERE, ICON_EMPTY, 1, DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_delivered_l1"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_delivered_l2"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_delivered_l3"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_returned_l1"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_returned_l2"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("power_returned_l3"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOWATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
         ),
         cv.Optional("reactive_power_delivered_l1"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_delivered_l2"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_delivered_l3"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_returned_l1"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_returned_l2"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("reactive_power_returned_l3"): sensor.sensor_schema(
-            UNIT_WATT, ICON_EMPTY, 3, DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT
+            UNIT_KILOVOLT_AMPS_REACTIVE,
+            ICON_EMPTY,
+            3,
+            DEVICE_CLASS_EMPTY,
+            STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("voltage_l1"): sensor.sensor_schema(
             UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE, STATE_CLASS_NONE
@@ -176,27 +266,25 @@ CONFIG_SCHEMA = cv.Schema(
             UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE, STATE_CLASS_NONE
         ),
         cv.Optional("gas_delivered"): sensor.sensor_schema(
-            "m³",
+            UNIT_CUBIC_METER,
             ICON_EMPTY,
             3,
-            DEVICE_CLASS_EMPTY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER,
+            DEVICE_CLASS_GAS,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
         cv.Optional("gas_delivered_be"): sensor.sensor_schema(
-            "m³",
+            UNIT_CUBIC_METER,
             ICON_EMPTY,
             3,
-            DEVICE_CLASS_EMPTY,
-            STATE_CLASS_MEASUREMENT,
-            LAST_RESET_TYPE_NEVER
+            DEVICE_CLASS_GAS,
+            STATE_CLASS_TOTAL_INCREASING,
         ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
-def to_code(config):
-    hub = yield cg.get_variable(config[CONF_DSMR_ID])
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_DSMR_ID])
 
     sensors = []
     for key, conf in config.items():
@@ -204,8 +292,11 @@ def to_code(config):
             continue
         id = conf.get("id")
         if id and id.type == sensor.Sensor:
-            s = yield sensor.new_sensor(conf)
+            s = await sensor.new_sensor(conf)
             cg.add(getattr(hub, f"set_{key}")(s))
             sensors.append(f"F({key})")
 
-    cg.add_define("DSMR_SENSOR_LIST(F, sep)", cg.RawExpression(" sep ".join(sensors)))
+    if sensors:
+        cg.add_define(
+            "DSMR_SENSOR_LIST(F, sep)", cg.RawExpression(" sep ".join(sensors))
+        )
--- components/dsmr/text_sensor.py
+++ components/dsmr/text_sensor.py
@@ -3,17 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import (
     CONF_ID,
-    CONF_SENSOR,
-    ICON_EMPTY,
-    UNIT_WATT_HOURS,
 )
-from . import DSMR, CONF_DSMR_ID
+from . import Dsmr, CONF_DSMR_ID
 
 AUTO_LOAD = ["dsmr"]
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_DSMR_ID): cv.use_id(DSMR),
+        cv.GenerateID(CONF_DSMR_ID): cv.use_id(Dsmr),
         cv.Optional("identification"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
@@ -34,7 +31,12 @@ CONFIG_SCHEMA = cv.Schema(
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
             }
         ),
-        cv.Optional("electricity_tariff"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional("equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+            }
+        ),
+        cv.Optional("breaker_status"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
             }
@@ -69,7 +71,12 @@ CONFIG_SCHEMA = cv.Schema(
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
             }
         ),
-        cv.Optional("slave_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+        cv.Optional("sub_equipment_id"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+            }
+        ),
+        cv.Optional("gas_delivered_text"): text_sensor.TEXT_SENSOR_SCHEMA.extend(
             {
                 cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
             }
@@ -78,8 +85,8 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(cv.COMPONENT_SCHEMA)
 
 
-def to_code(config):
-    hub = yield cg.get_variable(config[CONF_DSMR_ID])
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_DSMR_ID])
 
     text_sensors = []
     for key, conf in config.items():
@@ -88,10 +95,12 @@ def to_code(config):
         id = conf.get("id")
         if id and id.type == text_sensor.TextSensor:
             var = cg.new_Pvariable(conf[CONF_ID])
-            yield text_sensor.register_text_sensor(var, conf)
+            await text_sensor.register_text_sensor(var, conf)
             cg.add(getattr(hub, f"set_{key}")(var))
             text_sensors.append(f"F({key})")
 
-    cg.add_define(
-        "DSMR_TEXT_SENSOR_LIST(F, sep)", cg.RawExpression(" sep ".join(text_sensors))
-    )
+    if text_sensors:
+        cg.add_define(
+            "DSMR_TEXT_SENSOR_LIST(F, sep)",
+            cg.RawExpression(" sep ".join(text_sensors)),
+        )
